9733ea5e6f4ecffb10a1e78be9445c2cfde7a15c
findawayer <findawayer@gmail.com>
2020-12-30 06:57:45 +0900

    Purge deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,13 @@ import path from 'path';
 // Load aliases map BEFORE local imports
 // and aliased imports will be correctly resolved.
 import './helpers/module-alias';
+import { resolve } from './helpers/resolve';
 import init from './server/init';
 
 // Load environment variables
 // (In production build, Vercel will take care of those variables.)
 if (process.env.NODE_ENV === 'development') {
-  dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+  dotenv.config({ path: resolve('.env') });
 }
 
 // Initialize the server
