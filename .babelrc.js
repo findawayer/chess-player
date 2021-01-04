086ff31badf278b4246f4f8b23152b054dccdaf8
findawayer <findawayer@gmail.com>
2021-01-04 22:35:15 +0900

    Fix tree shaking

--- .babelrc.js
+++ .babelrc.js
@@ -1,30 +1,31 @@
 module.exports = {
   presets: ['next/babel'],
   plugins: [
-    // Resolve imports to top level directory for better tree shaking and DX.
+    // Transpile TypeScript decorator. (required for TypeGraphQL)
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'babel-plugin-parameter-decorator',
+    // Resolve imports to top level directory for better tree shaking.
     [
       'babel-plugin-transform-imports',
       {
         '@material-ui/core': {
-          transform: '@material-ui/core/esm/${member}',
+          transform: '@material-ui/core/${member}',
           preventFullImport: true,
         },
         '@material-ui/icons': {
-          transform: '@material-ui/icons/esm/${member}',
+          transform: '@material-ui/icons/${member}',
           preventFullImport: true,
         },
         '@material-ui/lab': {
-          transform: '@material-ui/lab/esm/${member}',
+          transform: '@material-ui/lab/${member}',
           preventFullImport: true,
         },
       },
     ],
-    // Support `const enum` in TypeScript.
-    [
-      'const-enum',
-      {
-        transform: 'constObject',
-      },
-    ],
+    // Tranpile `const enum`.
+    ['const-enum', { transform: 'constObject' }],
   ],
+  ignore: ['node_modules', 'public'],
 };
--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
-/* eslint-disable no-restricted-syntax */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig = {
-  webpack(config) {
-    // Use ts-loader for decorators support
-    for (const rule of config.module.rules) {
-      if (rule.test && rule.test.test('foo.ts')) {
-        rule.use = [].concat(rule.use, {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-          },
-        });
-      }
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = withBundleAnalyzer(nextConfig);
